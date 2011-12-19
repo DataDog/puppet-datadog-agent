9f6afebbda15af7c56d796287c8fcd5dd81813da
Rob Terhaar <rob@atlanticdynamic.com>
2011-12-19 16:02:13 -0500

    whitespace changes, and move the params class arg for dd_url out, for compat with puppet 2.6.1

--- manifests/init.pp
+++ manifests/init.pp
@@ -22,31 +22,33 @@
 #
 #
 class datadog(
-  $api_key = $datadog::params::api_key,
-  $dd_url  = $datadog::params::dd_url
+  $api_key = 'your key',
 ) inherits datadog::params {
 
-    case $operatingsystem {
-      "Ubuntu","Debian": { include datadog::ubuntu }
-      "RedHat","CentOS","Fedora": { include datadog::redhat }
-      default: { fail("The DataDog module only support Red Hat and Ubuntu derivatives") }
-    }
+  $dd_url  = $datadog::params::dd_url
 
-   file { "/etc/dd-agent":
-     ensure   => present,
-     owner    => "root",
-     group    => "root",
-     mode     => 0755,
-     require  => Package["datadog-agent"],
-   }
+  case $operatingsystem {
+    "Ubuntu","Debian": { include datadog::ubuntu }
+    "RedHat","CentOS","Fedora": { include datadog::redhat }
+    default: { fail("The DataDog module only support Red Hat and Ubuntu derivatives") }
+  }
 
-   file { "/etc/dd-agent/datadog.conf":
-     ensure   => present,
-     content  => template("datadog/datadog.conf.erb"),
-     owner    => "dd-agent",
-     group    => "root",
-     mode     => 0640,
-     notify   => Service["datadog-agent"],
-     require  => File["/etc/dd-agent"],
-   }
+  file { "/etc/dd-agent":
+    ensure   => present,
+    owner    => "root",
+    group    => "root",
+    mode     => 0755,
+    require  => Package["datadog-agent"],
+  }
+
+  file { "/etc/dd-agent/datadog.conf":
+    ensure   => present,
+    content  => template("datadog/datadog.conf.erb"),
+    owner    => "dd-agent",
+    group    => "root",
+    mode     => 0640,
+    notify   => Service["datadog-agent"],
+    require  => File["/etc/dd-agent"],
+  }
+
 }

2d264acb321bd3ee85ac114b056969fb638d85bd
Rob Terhaar <rob@atlanticdynamic.com>
2011-12-20 13:02:54 -0500

    adding a if defined() to install gems and dev for reports

--- manifests/reports.pp
+++ manifests/reports.pp
@@ -18,6 +18,33 @@ class datadog::reports(
   $puppetmaster_user
 ) {
 
+  include datadog::params
+  $rubygems_package = $datadog::params::rubygems_package
+  $rubydev_package =$datadog::params::rubydev_package
+
+  # check to make sure that you're not installing rubygems somewhere else,
+  # and install it if it's not defined elsewhere in your puppet catalog 
+  if defined(Package[$rubygems_package]) {
+    # pass
+    # puppet DSL lacks a 'not' in < 2.6.8
+  } else {
+    package {"$rubygems_package":
+      ensure => installed,
+      before => Package['dogapi'],
+    }
+  }
+
+  # check to make sure that you're not installing rubydev somewhere else
+  if defined(Package[$rubydev_package]) {
+    # pass
+    # puppet DSL lacks a 'not' in < 2.6.8
+  } else {
+    package {"$rubydev_package":
+      ensure => installed,
+      before => Package['dogapi'],
+    }
+  }
+
   file { "/etc/dd-agent/datadog.yaml":
     ensure   => file,
     content  => template("datadog/datadog.yaml.erb"),

6a0447dc072275037a090e13ee1decb9f3ae9f27
Alexandre Fouche <alexandre.fouche@gmail.com>
2014-01-09 14:48:57 +0100

    Add Jenkins integration

---
+++ manifests/integrations/jenkins.pp
@@ -0,0 +1,28 @@
+# Class: datadog::integrations::jenkins
+#
+# This class will install the necessary configuration for the jenkins integration
+#
+# Parameters:
+#   $path:
+#       Jenkins path. Defaults to '/var/lib/jenkins'
+#
+# Sample Usage:
+#
+#  class { 'datadog::integrations::jenkins' :
+#    path     => '/var/lib/jenkins',
+#  }
+#
+#
+class datadog::integrations::jenkins(
+  $path = '/var/lib/jenkins'
+) inherits datadog::params {
+
+  file { "${conf_dir}/jenkins.yaml":
+    ensure  => file,
+    owner   => $dd_user,
+    group   => $dd_group,
+    mode    => 0600,
+    content => template('datadog/integrations/jenkins.yaml.erb'),
+    notify  => Service[$service_name]
+  }
+}

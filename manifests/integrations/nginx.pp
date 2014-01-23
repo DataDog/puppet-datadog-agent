95f1efc39ef5d5ad4f6a3af57d84f6f79ded8952
Alexandre Fouche <alexandre.fouche@gmail.com>
2014-01-23 18:17:49 +0100

    nginx integration

---
+++ manifests/integrations/nginx.pp
@@ -0,0 +1,39 @@
+# Class: datadog::integrations::nginx
+#
+# This class will install the necessary configuration for the nginx integration
+#
+# Parameters:
+#   $instances:
+#       Array of hashes for all nginx urls and associates tags. See example
+#
+# Sample Usage:
+#
+#   class { 'datadog::integrations::nginx':
+#     instances => [
+#         {
+#             'nginx_status_url'  => 'http://example.com/nginx_status/',
+#         },
+#         {
+#             'nginx_status_url'  => 'http://example2.com:1234/nginx_status/',
+#             'tags' => ['instance:foo'],
+#         },
+#     ],
+#   }
+#
+#
+#
+class datadog::integrations::nginx(
+  $instances = [],
+) inherits datadog::params {
+
+  validate_array( $instances )
+
+  file { "${conf_dir}/nginx.yaml":
+    ensure  => file,
+    owner   => $dd_user,
+    group   => $dd_group,
+    mode    => 0600,
+    content => template('datadog/integrations/nginx.yaml.erb'),
+    notify  => Service[$service_name]
+  }
+}

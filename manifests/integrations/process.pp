06a5c91acd9a762a191a8eecff1a40577b7ceeeb
Alexandre Fouche <alexandre.fouche@gmail.com>
2014-01-23 18:16:40 +0100

    Process integration

---
+++ manifests/integrations/process.pp
@@ -0,0 +1,47 @@
+# Class: datadog::integrations::process
+#
+# This class will install the necessary configuration for the process integration
+#
+# Parameters:
+#   $processes:
+#       Array of process hashes. See example
+#
+# Sample Usage:
+#
+# class { 'datadog::integrations::process':
+#     processes   => [
+#         {
+#             'name'          => 'puppetmaster',
+#             'search_string' => ['puppet master'],
+#             'exact_match'   => false,
+#         },
+#         {
+#             'name'          => 'sshd',
+#             'search_string' => ['/usr/sbin/sshd'],
+#             'exact_match'   => true,
+#         },
+#     ],
+# }
+
+#
+#
+class datadog::integrations::process(
+  $processes = [],
+) inherits datadog::params {
+
+  validate_array( $processes )
+
+  package { $process_int_package :
+    ensure => installed,
+  }
+
+  file { "${conf_dir}/process.yaml":
+    ensure  => file,
+    owner   => $dd_user,
+    group   => $dd_group,
+    mode    => 0600,
+    content => template('datadog/integrations/process.yaml.erb'),
+    require => Package[$process_int_package],
+    notify  => Service[$service_name]
+  }
+}

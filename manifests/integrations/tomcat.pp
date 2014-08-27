dc8a95598c761457e6aaeab3ffe4c4d497542cc1
Etienne Pelletier <etienne.pelletier@powerreviews.com>
2014-08-27 10:55:36 -0700

    Added tomcat and solr integrations

---
+++ manifests/integrations/tomcat.pp
@@ -0,0 +1,21 @@
+class datadog_agent::integrations::tomcat(
+  $hostname             = 'localhost',
+  $port                 = 7199,
+  $username             = undef,
+  $password             = undef,
+  $java_bin_path        = undef,
+  $trust_store_path     = undef,
+  $trust_store_password = undef,
+  $tags                 = []) inherits datadog_agent::params {
+
+  file { "${conf_dir}/tomcat.yaml":
+    ensure  => file,
+    owner   => $datadog_agent::params::dd_user,
+    group   => $datadog_agent::params::dd_group,
+    mode    => 0600,
+    content => template('datadog_agent/agent-conf.d/tomcat.yaml.erb'),
+    require => Package[$datadog_agent::params::package_name],
+    notify  => Service[$datadog_agent::params::service_name]
+  }
+
+}

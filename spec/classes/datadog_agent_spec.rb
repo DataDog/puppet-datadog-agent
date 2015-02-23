a8107f609efa52626654d3aec8a3c42c51de6d2d
Léo Cavaillé <leo@datadoghq.com>
2015-02-23 12:37:55 -0500

    Add specs to cover all manifests
    Plus a few improvements in the manifests that were easy to catch
    with the specs:
    * no need to run apt-get update after adding the repo key
    * the redhat manifest should uninstall datadog-agent-base

--- spec/classes/datadog_agent_spec.rb
+++ spec/classes/datadog_agent_spec.rb
@@ -1,40 +1,54 @@
 require 'spec_helper'
 
 describe 'datadog_agent' do
-
-  context 'Debian based supported operating systems' do
-    ['Ubuntu', 'Debian'].each do |operatingsystem|
-      describe "datadog_agent class without any parameters on #{operatingsystem}" do
-        let(:params) {{ }}
-        let(:facts) {{
-          :operatingsystem => operatingsystem,
-        }}
-
-        it { should contain_class('datadog_agent::ubuntu') }
-      end
-    end
-  end
-
-  context 'Yum based supported operating systems' do
-      ['RedHat', 'CentOS', 'Fedora', 'Amazon', 'Scientific'].each do |operatingsystem|
-      describe "datadog_agent class without any parameters on #{operatingsystem}" do
-        let(:params) {{ }}
-        let(:facts) {{
-          :operatingsystem => operatingsystem,
-        }}
-
-        it { should contain_class('datadog_agent::redhat') }
-      end
-    end
-  end
   context 'unsupported operating system' do
     describe 'datadog_agent class without any parameters on Solaris/Nexenta' do
-      let(:facts) {{
-        :osfamily        => 'Solaris',
-        :operatingsystem => 'Nexenta',
-      }}
+      let(:facts) do
+        {
+          osfamily:         'Solaris',
+          operatingsystem:  'Nexenta'
+        }
+      end
 
-      it { expect { should contain_package('module') }.to raise_error(Puppet::Error, /Unsupported operatingsystem: Nexenta/) }
+      it do
+        expect {
+          should contain_package('module')
+        }.to raise_error(Puppet::Error, /Unsupported operatingsystem: Nexenta/)
+      end
+    end
+  end
+
+  # Test all supported OSes
+  context 'all supported operating systems' do
+    ALL_OS.each do |operatingsystem|
+      describe "datadog_agent class common actions on #{operatingsystem}" do
+        let(:params) { { puppet_run_reports: true } }
+        let(:facts) do
+          {
+            operatingsystem: operatingsystem,
+            osfamily: DEBIAN_OS.include?(operatingsystem) ? 'debian' : 'redhat'
+          }
+        end
+
+        it { should compile.with_all_deps }
+
+        it { should contain_class('datadog_agent') }
+
+        describe 'datadog_agent imports the default params' do
+          it { should contain_class('datadog_agent::params') }
+        end
+
+        it { should contain_file('/etc/dd-agent') }
+        it { should contain_file('/etc/dd-agent/datadog.conf') }
+
+        it { should contain_class('datadog_agent::reports') }
+
+        if DEBIAN_OS.include?(operatingsystem)
+          it { should contain_class('datadog_agent::ubuntu') }
+        elsif REDHAT_OS.include?(operatingsystem)
+          it { should contain_class('datadog_agent::redhat') }
+        end
+      end
     end
   end
 end

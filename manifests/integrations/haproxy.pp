8828d6010a54ee39dfc1dae18cad8a84e9aa20d1
Rhommel Lamas <roml@rhommell.com>
2015-05-13 12:50:33 +1000

    Add haproxy integration

---
+++ manifests/integrations/haproxy.pp
@@ -0,0 +1,33 @@
+# Class: datadog_agent::integrations::haproxy
+#
+# This class will install the necessary configuration for the haproxy integration
+#
+# Parameters:
+#   $url:
+#     The URL for haproxy
+#
+# Sample Usage:
+#
+#   class { 'datadog_agent::integrations::haproxy' :
+#     url   => 'http://localhost:8080',
+#     creds => { username => 'admin',
+#                password => 'password',
+#              },
+#   }
+#
+class datadog_agent::integrations::haproxy(
+  $creds = {},
+  $url   = "http://${::ipaddress}:8080",
+) inherits datadog_agent::params {
+
+  file {
+    "${datadog_agent::params::conf_dir}/haproxy.yaml":
+      ensure  => file,
+      owner   => $datadog_agent::params::dd_user,
+      group   => $datadog_agent::params::dd_group,
+      mode    => '0644',
+      content => template('datadog_agent/agent-conf.d/haproxy.yaml.erb'),
+      require => Package[$datadog_agent::params::package_name],
+      notify  => Service[$datadog_agent::params::service_name]
+  }
+}

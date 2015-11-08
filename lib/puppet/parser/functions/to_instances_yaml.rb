26ae68e628896b825d5f3a093e1db800b2875568
Colin Wood <cwood06@gmail.com>
2015-11-07 19:03:39 -0800

    Change to string

--- lib/puppet/parser/functions/to_instances_yaml.rb
+++ lib/puppet/parser/functions/to_instances_yaml.rb
@@ -4,8 +4,8 @@ module Puppet::Parser::Functions
     newfunction(:to_instances_yaml, :type => :rvalue) do |args|
         ruby_data = args[0]
         default_values = {
-            :init_config  => nil,
-            :instances => ruby_data
+            'init_config'  => nil,
+            'instances' => ruby_data
         }
         YAML::dump(default_values)
     end

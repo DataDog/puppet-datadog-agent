95c6b0900c82873befb2cc77002aa0039d1cbf41
Alan Scherger <ascherger@homeaway.com>
2015-12-03 00:31:41 -0600

    add dd user to docker group

--- manifests/integrations/docker.pp
+++ manifests/integrations/docker.pp
@@ -12,6 +12,10 @@
 #   $tags:
 #     optional array of tags
 #
+#   $group:
+#     optional name of docker group to add dd-agent user too
+#
+#
 # Sample Usage:
 #
 #   class { 'datadog_agent::integrations::docker' :
@@ -23,7 +27,15 @@ class datadog_agent::integrations::docker(
   $new_tag_names = true,
   $url = 'unix://var/run/docker.sock',
   $tags = [],
+  $group = 'docker',
 ) inherits datadog_agent::params {
+
+  exec { "dd-agent-should-be-in-docker-group":
+    command => "/usr/sbin/usermod -aG ${group} ${datadog_agent::params::dd_user}",
+    unless  => "/bin/cat /etc/group | grep '^${group}:' | grep -qw ${datadog_agent::params::dd_user}",
+    require => Package[$datadog_agent::params::package_name],
+    notify  => Service[$datadog_agent::params::service_name]
+  }
 
   file { "${datadog_agent::params::conf_dir}/docker.yaml":
     ensure  => file,

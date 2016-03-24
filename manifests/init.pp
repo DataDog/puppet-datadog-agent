1135e1f52bef64bbf550c6273ff3a73d754c0516
Colin Wood <cwood06@gmail.com>
2016-03-24 13:17:05 -0700

    Fix some typos

--- manifests/init.pp
+++ manifests/init.pp
@@ -183,7 +183,7 @@ class datadog_agent(
   $collect_ec2_tags = false,
   $collect_instance_metadata = true,
   $tags = [],
-  $intergrations = {},
+  $integrations = {},
   $hiera_integrations = false,
   $hiera_tags = false,
   $facts_to_tags = [],
@@ -392,5 +392,6 @@ class datadog_agent(
       hostname_extraction_regex => $hostname_extraction_regex,
     }
   }
-  create_resources('datadog_agent::intergration', $local_integrations)
+
+  create_resources('datadog_agent::integration', $local_integrations)
 }

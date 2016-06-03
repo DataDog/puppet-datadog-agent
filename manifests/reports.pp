5521a565cc63cf7f443a070d8e83c4cbf7ccd731
Jaime Fullaondo <jaime.fullaondo@datadoghq.com>
2016-06-03 18:31:30 -0400

    [puppet] PE and POSS require puppetserver_gem to get the dogapi gem in the JRuby env.

--- manifests/reports.pp
+++ manifests/reports.pp
@@ -21,6 +21,13 @@ class datadog_agent::reports(
 
   include datadog_agent::params
   $rubydev_package = $datadog_agent::params::rubydev_package
+  $gemprovider = 'puppetserver_gem'
+
+  # set the right provider
+  # NOTE: should we also check for clientversion?
+  if (!defined('$::serverversion') or versioncmp($::serverversion, '3.7.0') < 0) {
+      $gemprovider = 'gem'
+  }
 
   # check to make sure that you're not installing rubydev somewhere else
   if ! defined(Package[$rubydev_package]) {
@@ -48,7 +55,7 @@ class datadog_agent::reports(
 
   package{'dogapi':
     ensure   => 'installed',
-    provider => 'gem',
+    provider => $gemprovider,
   }
 
 }

aa7cd25c40323d508e88e352c9fef570ab8fb532
Petter Abrahamsson <petter@jebus.nu>
2016-06-29 14:23:16 -0400

    Add Elasticsearch shield support

--- manifests/integrations/elasticsearch.pp
+++ manifests/integrations/elasticsearch.pp
@@ -13,9 +13,30 @@
 #   }
 #
 class datadog_agent::integrations::elasticsearch(
-  $url = 'http://localhost:9200'
+  $cluster_stats      = false,
+  $password           = undef,
+  $pending_task_stats = true,
+  $pshard_stats       = false,
+  $ssl_cert           = undef,
+  $ssl_key            = undef,
+  $ssl_verify         = true,
+  $tags               = [],
+  $url                = 'http://localhost:9200',
+  $username           = undef,
 ) inherits datadog_agent::params {
   include datadog_agent
+
+  validate_array($tags)
+  # $ssl_verify can be a bool or a string
+  # https://github.com/DataDog/dd-agent/blob/master/checks.d/elastic.py#L454-L455
+  if is_bool($ssl_verify) {
+    validate_bool($ssl_verify)
+  } elsif $ssl_verify != undef {
+    validate_string($ssl_verify)
+    validate_absolute_path($ssl_verify)
+  }
+  validate_bool($cluster_stats, $pending_task_stats, $pshard_stats)
+  validate_string($password, $ssl_cert, $ssl_key, $url, $username)
 
   file { "${datadog_agent::params::conf_dir}/elastic.yaml":
     ensure  => file,

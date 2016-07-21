7ea03ffc5a64390f83159b8c18af4b80b2af8ffb
Colin Wood <cwood06@gmail.com>
2016-07-21 13:29:41 -0700

    Allow looking up facts to tags

--- manifests/tag.pp
+++ manifests/tag.pp
@@ -1,10 +1,20 @@
 # Allow custom tags via a define
 define datadog_agent::tag(
   $tag = $name,
+  $lookup_fact = false,
 ){
-  concat::fragment{ "datadog tag ${tag}":
-    target  => '/etc/dd-agent/datadog.conf',
-    content => "${tag}, ",
-    order   => '03',
+  unless $lookup_fact {
+    concat::fragment{ "datadog tag ${tag}":
+      target  => '/etc/dd-agent/datadog.conf',
+      content => "${tag}, ",
+      order   => '03',
+    }
+  } else {
+    $value = getvar($tag)
+    concat::fragment{ "datadog tag ${tag}":
+      target  => '/etc/dd-agent/datadog.conf',
+      content => "${tag}:${value}, ",
+      order   => '03',
+    }
   }
 }

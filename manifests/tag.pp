31deadd350fa7f4a2f5ca7bb40712d11f175d40b
Colin Wood <cwood06@gmail.com>
2016-07-21 13:33:14 -0700

    Fix unless issue

--- manifests/tag.pp
+++ manifests/tag.pp
@@ -3,18 +3,20 @@ define datadog_agent::tag(
   $tag = $name,
   $lookup_fact = false,
 ){
-  unless $lookup_fact {
+
+  if $lookup_fact{
+    $value = getvar($tag)
+    concat::fragment{ "datadog tag ${tag}:${value}":
+      target  => '/etc/dd-agent/datadog.conf',
+      content => "${tag}:${value}, ",
+      order   => '03',
+    }
+  } else {
     concat::fragment{ "datadog tag ${tag}":
       target  => '/etc/dd-agent/datadog.conf',
       content => "${tag}, ",
       order   => '03',
     }
-  } else {
-    $value = getvar($tag)
-    concat::fragment{ "datadog tag ${tag}":
-      target  => '/etc/dd-agent/datadog.conf',
-      content => "${tag}:${value}, ",
-      order   => '03',
-    }
   }
+
 }

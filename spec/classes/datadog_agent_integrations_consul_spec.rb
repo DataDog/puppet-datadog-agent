f85ab17f0c8d67e95f150d211944cf0bc2e9d8e1
Alan Scherger <alan.scherger@gmail.com>
2016-11-30 11:36:17 -0600

    add tests for consul integration

---
+++ spec/classes/datadog_agent_integrations_consul_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+
+describe 'datadog_agent::integrations::consul' do
+  let(:facts) {{
+    operatingsystem: 'Ubuntu',
+  }}
+  let(:conf_dir) { '/etc/dd-agent/conf.d' }
+  let(:dd_user) { 'dd-agent' }
+  let(:dd_group) { 'root' }
+  let(:dd_package) { 'datadog-agent' }
+  let(:dd_service) { 'datadog-agent' }
+  let(:conf_file) { "#{conf_dir}/consul.yaml" }
+
+  it { should compile.with_all_deps }
+  it { should contain_file(conf_file).with(
+    owner: dd_user,
+    group: dd_group,
+    mode: '0644',
+  )}
+  it { should contain_file(conf_file).that_requires("Package[#{dd_package}]") }
+  it { should contain_file(conf_file).that_notifies("Service[#{dd_service}]") }
+
+end

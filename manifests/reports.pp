5b7dc12a4ead2d35340bf5b3d7cec16ccdecc073
Jon Evans <jon.evans@ixxus.com>
2016-12-22 16:38:41 +0000

    Fix more nil / undef confusion

--- manifests/reports.pp
+++ manifests/reports.pp
@@ -17,7 +17,7 @@ class datadog_agent::reports(
   $api_key,
   $puppetmaster_user,
   $dogapi_version,
-  $hostname_extraction_regex = nil,
+  $hostname_extraction_regex = undef
   $datadog_site = 'datadoghq.com',
 ) {
 

cffc2dc8be202525ddeea0cd5156a3fbb2ea0999
Adrian Doolittle <adrian.doolittle@gmail.com>
2017-02-21 17:46:43 -0800

    typo fixing

--- manifests/init.pp
+++ manifests/init.pp
@@ -147,7 +147,7 @@ class datadog_agent(
     }
   }
 
-  exec { 'rename_default_configs'
+  exec { 'rename_default_configs':
     command => 'find /etc/dd-agent/conf.d -iname "*.yaml.default" -exec rename yaml.default yaml "{}" \;',
     onlyif  => 'ls /etc/dd-agent/conf.d/*default',
   }

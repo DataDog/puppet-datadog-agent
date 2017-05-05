aae4cf86c6d629f078a263ec0fc24a5a148478d3
Stanislav Makar <stamak@users.noreply.github.com>
2017-05-06 00:27:44 +0300

    Add parameters to ceph integration (#322)
    Closes: #321

--- spec/classes/datadog_agent_integrations_ceph_spec.rb
+++ spec/classes/datadog_agent_integrations_ceph_spec.rb
@@ -28,4 +28,24 @@ describe 'datadog_agent::integrations::ceph' do
     it { should contain_file(sudo_conf_file).with_content(/^dd-agent\sALL=.*NOPASSWD:\/usr\/bin\/ceph$/) }
   end
 
+  context 'with specified tag' do
+    let(:params) {{
+      tags: ['name:my_ceph_cluster'],
+    }}
+    it { should contain_file(conf_file).with_content(/tags:\s+- name:my_ceph_cluster\s*?[^-]/m) }
+    it { should contain_file(conf_file).with_content(/^\s*ceph_cmd:\s*\/usr\/bin\/ceph\s*?[^-]/m) }
+    it { should contain_file(conf_file).with_content(/^\s*use_sudo:\sTrue$/) }
+    it { should contain_file(sudo_conf_file).with_content(/^dd-agent\sALL=.*NOPASSWD:\/usr\/bin\/ceph$/) }
+  end
+
+  context 'with specified ceph_cmd' do
+    let(:params) {{
+      ceph_cmd: '/usr/local/myceph',
+    }}
+    it { should contain_file(conf_file).with_content(/tags:\s+- name:ceph_cluster\s*?[^-]/m) }
+    it { should contain_file(conf_file).with_content(/^\s*ceph_cmd:\s*\/usr\/local\/myceph\s*?[^-]/m) }
+    it { should contain_file(conf_file).with_content(/^\s*use_sudo:\sTrue$/) }
+    it { should contain_file(sudo_conf_file).with_content(/^dd-agent\sALL=.*NOPASSWD:\/usr\/bin\/ceph$/) }
+  end
+
 end

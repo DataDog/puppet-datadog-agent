48210c4bf17f72090d72d3a5aad547550f779b99
Wolf Wolfschuetz <wolf@shapeways.com>
2017-05-11 14:05:02 +0200

    add options to haproxy integration

--- spec/classes/datadog_agent_integrations_haproxy_spec.rb
+++ spec/classes/datadog_agent_integrations_haproxy_spec.rb
@@ -55,30 +55,47 @@ describe 'datadog_agent::integrations::haproxy' do
     end
   end
 
+  context 'with options set' do
+    let(:params) {{
+      options: {
+        'optionk' => 'optionv',
+      },
+    }}
+    it { should contain_file(conf_file).with_content(%r{optionk: optionv}) }
+  end
+
   context 'with instances set' do
     let(:params) {{
       instances: [
         {
-          'url'   => 'http://foo.bar:8421',
-          'creds' => {
+          'url'     => 'http://foo.bar:8421',
+          'creds'   => {
             'username' => 'foo',
             'password' => 'bar',
-          }
+          },
+          'options' => {
+            'optionk1' => 'optionv1',
+          },
         },
         {
-          'url'   => 'http://shoe.baz:1248',
-          'creds' => {
+          'url'     => 'http://shoe.baz:1248',
+          'creds'   => {
             'username' => 'shoe',
             'password' => 'baz',
-          }
+          },
+          'options' => {
+            'optionk2' => 'optionv2',
+          },
         },
       ]
     }}
     it { should contain_file(conf_file).with_content(%r{url: http://foo.bar:8421}) }
     it { should contain_file(conf_file).with_content(%r{username: foo}) }
     it { should contain_file(conf_file).with_content(%r{password: bar}) }
+    it { should contain_file(conf_file).with_content(%r{optionk1: optionv1}) }
     it { should contain_file(conf_file).with_content(%r{url: http://shoe.baz:1248}) }
     it { should contain_file(conf_file).with_content(%r{username: shoe}) }
     it { should contain_file(conf_file).with_content(%r{password: baz}) }
+    it { should contain_file(conf_file).with_content(%r{optionk2: optionv2}) }
   end
 end

9df65af894287a70692edf52bda9c692ef665715
Alexandre Fouche <alexandre.fouche@gmail.com>
2017-10-22 15:39:54 +0200

    linux_proc_extras integration monitoring

---
+++ manifests/integrations/linux_proc_extras.pp
@@ -0,0 +1,31 @@
+# Class: datadog_agent::integrations::system_core
+#
+# This class will install the necessary configuration for the CPU cores integration
+#
+# Parameters:
+#
+#   tags
+#       The (optional) tags to add to the check instance.
+#
+# Sample Usage:
+#
+#  class { 'datadog_agent::integrations::linux_proc_extras':
+#      tags => [ 'env:production' ],
+#  }
+
+class datadog_agent::integrations::linux_proc_extras(
+  $tags = [],
+) inherits datadog_agent::params {
+  include datadog_agent
+
+  file { "${datadog_agent::params::conf_dir}/linux_proc_extras.yaml":
+    ensure  => file,
+    owner   => $datadog_agent::params::dd_user,
+    group   => $datadog_agent::params::dd_group,
+    mode    => '0600',
+    content => template('datadog_agent/agent-conf.d/linux_proc_extras.yaml.erb'),
+    require => Package[$datadog_agent::params::package_name],
+    notify  => Service[$datadog_agent::params::service_name]
+  }
+
+}

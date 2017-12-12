235169763ef6797617a5de7df71a9562116efb25
Jaime Fullaondo <truthbk@gmail.com>
2017-12-12 12:40:42 +0100

    [apt] Only grep for last 8 characters as this is what apt-key list returns (#374)
    * Only grep for last 8 characters as this is what apt-key list returns
    
    * [ubuntu] fix key substring extraction-interpolation
    
    [key] get last eight characters with regex subst

--- manifests/ubuntu/install_key.pp
+++ manifests/ubuntu/install_key.pp
@@ -13,8 +13,10 @@
 #
 #
 define datadog_agent::ubuntu::install_key() {
+  $shortkey =  regsubst($name, '.*(.{8})$', '\1')
+
   exec { "key ${name}":
     command => "/usr/bin/apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys ${name}",
-    unless  => "/usr/bin/apt-key list | grep ${name} | grep expires",
+    unless  => "/usr/bin/apt-key list | grep ${shortkey} | grep expires",
   }
 }

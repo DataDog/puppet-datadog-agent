bf1a806f1f2ff9949c884e24fb574f5ca5714d91
Jaime Fullaondo <jaime.fullaondo@datadoghq.com>
2018-03-05 13:55:36 +0100

    [functions] fix typo in tag6, use variable in recursion

--- functions/tag6.pp
+++ functions/tag6.pp
@@ -8,7 +8,7 @@ function datadog_agent::tag6(
 ) {
   if $tag_names =~ Array {
     $tags = $tag_names.reduce([]) |$_tags , $tag| {
-        concat($_tags, datadog_agent::tag6($tag, lookup_fact))
+        concat($_tags, datadog_agent::tag6($tag, $lookup_fact))
     }
   } else {
     if $lookup_fact =~ String {

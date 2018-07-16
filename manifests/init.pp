fc583398c91e5c5ed27ad8a1f51320a8a5decd0b
Bill Medernach <bill.medernach@adroll.com>
2018-07-16 11:56:48 -0700

    WIP

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,6 +1,6 @@
 class datadog_agent {
+  $agent_version       = 'latest'
   $repo_uri            = "https://yum.datadoghq.com/stable/6/${::architecture}/"
-  $conf6_dir           = '/etc/datadog-agent/conf.d'
   $dd_user             = 'dd-agent'
   $dd_group            = 'root'
   $package_name        = 'datadog-agent'
@@ -8,15 +8,43 @@ class datadog_agent {
   $service_ensure      = 'running'
   $service_enable      = true
 
-  class { 'datadog_agent::redhat::agent6':
-    baseurl          => $repo_uri,
-    manage_repo      => $manage_repo,
-    agent_version    => $agent_version,
-    service_ensure   => $service_ensure,
-    service_enable   => $service_enable,
-    service_provider => $service_provider,
+  yumrepo {'datadog':
+    ensure   => absent,
   }
 
+  yumrepo {'datadog5':
+    ensure   => absent,
+  }
+
+  yumrepo {'datadog6':
+    enabled  => 1,
+    gpgcheck => 0,
+    descr    => 'Datadog, Inc.',
+    baseurl  => $repo_uri
+  }
+  
+  Package { require => Yumrepo['datadog6']}
+
+  package { 'datadog-agent-base':
+    ensure => absent,
+    before => Package[$package_name],
+  }
+
+  package { $package_name:
+    ensure  => $agent_version,
+  }
+
+  service { $service_name:
+    ensure    => $service_ensure,
+    hasstatus => false,
+    pattern   => 'dd-agent',
+    require   => Package[$package_name],
+    start     => 'initctl start datadog-agent || true',
+    stop      => 'initctl stop datadog-agent || true',
+    status    => 'initctl status datadog-agent || true',
+    restart   => 'initctl restart datadog-agent || true',
+  }
+  
   file { '/etc/datadog-agent':
     ensure  => directory,
     purge   => false,
@@ -24,49 +52,48 @@ class datadog_agent {
     force   => false,
     owner   => $dd_user,
     group   => $dd_group,
-    notify  => Service[$datadog_agent::params::service_name]
+    notify  => Service[$service_name]
   }
 
   file { '/etc/datadog-agent/conf.d/carabiner.d':
     ensure  => directory,
-    purge   => $false,
+    purge   => false,
     recurse => true,
-    force   => $false,
+    force   => false,
     owner   => $dd_user,
     group   => $dd_group,
   }
   
   file { '/etc/datadog-agent/conf.d/system_logs.d':
     ensure  => directory,
-    purge   => $false,
+    purge   => false,
     recurse => true,
-    force   => $false,
+    force   => false,
     owner   => $dd_user,
     group   => $dd_group,
   }
 
   file { '/etc/datadog-agent/datadog.yaml':
-      owner   => 'dd-agent',
-      group   => 'dd-agent',
-      mode    => '0640',
-      ensure => present,
-      source => 'puppet:///modules/datadog_agent/datadog.yaml',
-      notify  => Service[$datadog_agent::params::service_name],
-      require => Package['datadog-agent'];
-    }
+    owner   => 'dd-agent',
+    group   => 'dd-agent',
+    mode    => '0640',
+    ensure => present,
+    source => 'puppet:///modules/datadog_agent/datadog.yaml',
+    notify  => Service[$service_name],
+    require => Package['datadog-agent'];
   }
 
   file { '/etc/datadog-agent/conf.d/carabiner.d/conf.yaml':
     ensure => present,
     source => 'puppet:///modules/datadog_agent/carabiner_conf.yaml',
-    notify  => Service[$datadog_agent::params::service_name],
+    notify  => Service[$service_name],
     require => Package['datadog-agent'];
   }
   
   file { '/etc/datadog-agent/conf.d/system_logs.d/conf.yaml':
     ensure => present,
     source => 'puppet:///modules/datadog_agent/system_logs_conf.yaml',
-    notify  => Service[$datadog_agent::params::service_name],
+    notify  => Service[$service_name],
     require => Package['datadog-agent'];
   }
 

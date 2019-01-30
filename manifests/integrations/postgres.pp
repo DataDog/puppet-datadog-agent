d351acbea44a1b478f6260f8d8004006282ca6f0
Diogo Kiss <dki@tradeshift.com>
2019-01-30 11:43:50 +0100

    Make it possible to enable PostgreSQL extra metrics collection
    The current version of the PostgresSQL module integration doesn't
    handle the following parameters for the Datadog PostgreSQL integration:
      * collect_function_metrics
      * collect_count_metrics
      * collect_activity_metrics
      * collect_database_size_metrics
    
    This way metrics like postgresql.active_queries and postgresql.waiting_queries,
    for example, were not available.
    This change aims to make them available whenever desired.

--- manifests/integrations/postgres.pp
+++ manifests/integrations/postgres.pp
@@ -5,7 +5,7 @@
 # Parameters:
 #   $password
 #       The password for the datadog user
-#   $host:
+#   $host
 #       The host postgres is running on
 #   $dbname
 #       The postgres database name
@@ -18,6 +18,18 @@
 #   $use_psycopg2
 #       Boolean to flag connecting to postgres with psycopg2 instead of pg8000.
 #       Warning, psycopg2 doesn't support ssl mode.
+#   $collect_function_metrics
+#       Boolean to enable collecting metrics regarding PL/pgSQL functions from pg_stat_user_functions.
+#   $collect_count_metrics
+#       Boolean to enable collecting count metrics, default value is True for backward compatibility but they might be slow,
+#       suggested value is False.
+#   $collect_activity_metrics
+#       Boolean to enable collecting metrics regarding transactions from pg_stat_activity, default value is False.
+#       Please make sure the user has sufficient privileges to read from pg_stat_activity before enabling this option.
+#   $collect_database_size_metrics
+#       Boolean to enable collecting database size metrics. Default value is True but they might be slow with large databases
+#   $collect_default_database
+#       Boolean to enable collecting statistics from the default database 'postgres' in the check metrics, default to false
 #   $tags
 #       Optional array of tags
 #   $tables
@@ -56,16 +68,21 @@
 #
 class datadog_agent::integrations::postgres(
   String $password,
-  String $host                   = 'localhost',
-  String $dbname                 = 'postgres',
-  Variant[String, Integer] $port = '5432',
-  String $username               = 'datadog',
-  Boolean $ssl                   = false,
-  Boolean $use_psycopg2          = false,
-  Array[String] $tags            = [],
-  Array[String] $tables          = [],
-  Hash $custom_metrics           = {},
-  Optional[Array] $instances     = undef,
+  String $host                           = 'localhost',
+  String $dbname                         = 'postgres',
+  Variant[String, Integer] $port         = '5432',
+  String $username                       = 'datadog',
+  Boolean $ssl                           = false,
+  Boolean $use_psycopg2                  = false,
+  Boolean $collect_function_metrics      = false,
+  Boolean $collect_count_metrics         = false,
+  Boolean $collect_activity_metrics      = false,
+  Boolean $collect_database_size_metrics = false,
+  Boolean $collect_default_database      = false,
+  Array[String] $tags                    = [],
+  Array[String] $tables                  = [],
+  Hash $custom_metrics                   = {},
+  Optional[Array] $instances             = undef,
 ) inherits datadog_agent::params {
   include datadog_agent
 
@@ -85,16 +102,21 @@ class datadog_agent::integrations::postgres(
 
   if !$instances and $host {
     $_instances = [{
-      'host'           => $host,
-      'password'       => $password,
-      'dbname'         => $dbname,
-      'port'           => $port,
-      'username'       => $username,
-      'ssl'            => $ssl,
-      'use_psycopg2'   => $use_psycopg2,
-      'tags'           => $tags,
-      'tables'         => $tables,
-      'custom_metrics' => $custom_metrics,
+      'host'                          => $host,
+      'password'                      => $password,
+      'dbname'                        => $dbname,
+      'port'                          => $port,
+      'username'                      => $username,
+      'ssl'                           => $ssl,
+      'use_psycopg2'                  => $use_psycopg2,
+      'tags'                          => $tags,
+      'tables'                        => $tables,
+      'custom_metrics'                => $custom_metrics,
+      'collect_function_metrics'      => $collect_function_metrics,
+      'collect_count_metrics'         => $collect_count_metrics,
+      'collect_activity_metrics'      => $collect_activity_metrics,
+      'collect_database_size_metrics' => $collect_database_size_metrics,
+      'collect_default_database'      => $collect_default_database,
     }]
   } elsif !$instances{
     $_instances = []

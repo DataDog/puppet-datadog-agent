2af31d8e7e6f764af0cbc55bc80374afffe38554
zickzackv <fabian.otto@gmail.com>
2019-04-18 15:29:08 +0200

    Custom integration with logs collection (#513)
    * adding third parameter to to_instances_yaml
    
    * introducing an optional third key logs for integration class

--- lib/puppet/parser/functions/to_instances_yaml.rb
+++ lib/puppet/parser/functions/to_instances_yaml.rb
@@ -4,10 +4,14 @@ module Puppet::Parser::Functions
     newfunction(:to_instances_yaml, :type => :rvalue) do |args|
         init_config = args[0]
         instances = args[1]
+        logs = args[2]
         default_values = {
             'init_config'  => init_config.is_a?(String) ? nil: init_config,
             'instances' => instances
         }
+        if !logs.nil? && !logs.empty? then
+          default_values.merge({'logs' => logs})
+        end
         YAML::dump(default_values)
     end
 end

0c833f0229d9aba3dd3a0ec26d00d169e4fcac89
Albert Vaca <albertvaka@gmail.com>
2019-08-22 12:24:23 +0200

    Fix formatting

--- manifests/windows/agent6.pp
+++ manifests/windows/agent6.pp
@@ -1,6 +1,6 @@
 # Class: datadog_agent::windows::agent6
 #
-# This class contains the DataDog agent installation mechanism for Windows derivatives
+# This class contains the DataDog agent installation mechanism for Windows
 #
 
 class datadog_agent::windows::agent6(
@@ -16,49 +16,49 @@ class datadog_agent::windows::agent6(
   Boolean $should_install = $datadog_agent::should_install_win
 ) inherits datadog_agent::params {
 
-	if $should_install {
-	  file { $msi_destination:
-      ensure        => directory,
-      owner         => 'Administrator',
-      group         => 'Administrators',
-      notify        => Exec['downloadmsi']
-  	}
+  if $should_install {
+    file { $msi_destination:
+      ensure => directory,
+      owner  => 'Administrator',
+      group  => 'Administrators',
+      notify => Exec['downloadmsi']
+    }
 
     exec { 'downloadmsi':
-      command       => "Invoke-WebRequest $baseurl -outfile $msi",
-      onlyif        => "if ((test-path $msi) -eq \$true) {exit 1}",
-      provider      => powershell,
-      notify        => Package['Datadog Agent']
+      command  => "Invoke-WebRequest ${baseurl} -outfile ${msi}",
+      onlyif   => "if ((test-path ${msi}) -eq \$true) {exit 1}",
+      provider => powershell,
+      notify   => Package['Datadog Agent']
     }
 
     package { 'Datadog Agent':
-      ensure            => installed,
-      provider          => 'windows',
-      source            => "$msi",
-      install_options   => ['/quiet', {'APIKEY' => $api_key, 'HOSTNAME' => $hostname, 'TAGS' => $tags}]
+      ensure          => installed,
+      provider        => 'windows',
+      source          => $msi,
+      install_options => ['/quiet', {'APIKEY' => $api_key, 'HOSTNAME' => $hostname, 'TAGS' => $tags}]
     }
 
     exec { 'DatadogRestart':
-      command           => "& 'C:/Program Files/Datadog/Datadog Agent/embedded/agent.exe' restart-service",
-      provider          => powershell
+      command  => "& 'C:/Program Files/Datadog/Datadog Agent/embedded/agent.exe' restart-service",
+      provider => powershell
     }
 
     service { $service_name:
-      ensure        => $service_ensure,
-      enable        => $service_enable,
-      require       => Package['Datadog Agent']
+      ensure  => $service_ensure,
+      enable  => $service_enable,
+      require => Package['Datadog Agent']
     }
-	} else {
+  } else {
     file { $msi_destination:
-      ensure        => absent,
-      owner         => 'Administrator',
-      group         => 'Administrators',
-      notify        => Package['Datadog Agent']
+      ensure => absent,
+      owner  => 'Administrator',
+      group  => 'Administrators',
+      notify => Package['Datadog Agent']
     }
 
     package { 'Datadog Agent':
-      ensure                 => absent,
-      uninstall_options      => ['/quiet']
+      ensure            => absent,
+      uninstall_options => ['/quiet']
     }
   }
 }

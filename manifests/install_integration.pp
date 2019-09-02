a455a8ecc47479c469c99b8115ba60e1d5eba620
Albert Vaca <albertvaka@gmail.com>
2019-09-02 17:10:58 +0200

    Windows support in install_integration

--- manifests/install_integration.pp
+++ manifests/install_integration.pp
@@ -6,19 +6,25 @@ define datadog_agent::install_integration (
 
   include datadog_agent
 
+  if $::operatingsystem == 'Windows' {
+    $agent_binary = 'C:/Program Files/Datadog/Datadog Agent/embedded/agent.exe'
+  } else {
+    $agent_binary = '/opt/datadog-agent/bin/agent/agent'
+  }
+
   if $ensure == 'present' {
     exec { "install ${integration_name}==${version}":
-      command => "/opt/datadog-agent/bin/agent/agent integration install ${integration_name}==${version}",
-      user    => 'dd-agent',
-      unless  => "/opt/datadog-agent/bin/agent/agent integration freeze | grep ${integration_name}==${version}",
+      command => "${agent_binary} integration install ${integration_name}==${version}",
+      user    => $datadog_agent::dd_user,
+      unless  => "${agent_binary} integration freeze | grep ${integration_name}==${version}",
       require => Package[$datadog_agent::params::package_name],
       notify  => Service[$datadog_agent::params::service_name],
     }
   } else {
     exec { "remove ${integration_name}==${version}":
-      command => "/opt/datadog-agent/bin/agent/agent integration remove ${integration_name}==${version}",
-      user    => 'dd-agent',
-      onlyif  => "/opt/datadog-agent/bin/agent/agent integration freeze | grep ${integration_name}==${version}",
+      command => "${agent_binary} integration remove ${integration_name}==${version}",
+      user    => $datadog_agent::dd_user,
+      onlyif  => "${agent_binary} integration freeze | grep ${integration_name}==${version}",
       require => Package[$datadog_agent::params::package_name],
       notify  => Service[$datadog_agent::params::service_name],
     }

54b2270f670543a21f15224439e0a9e226f347cf
Andre Sencioles <asenci@gmail.com>
2019-11-29 22:27:44 +1300

    Add Network Performance Monitoring support (#584)
    Disabled by default, can be enabled by instantializing the datadog_agent::system_probe class (process_enabled needs to be enabled on datadog_agent).

---
+++ manifests/system_probe.pp
@@ -0,0 +1,53 @@
+include datadog_agent::params
+class datadog_agent::system_probe(
+  Boolean $enabled = false,
+  Optional[String] $log_file = undef,
+  Optional[String] $sysprobe_socket = undef,
+
+  Boolean $service_enable = true,
+  String $service_ensure = 'running',
+  Optional[String] $service_provider = undef,
+) inherits datadog_agent::params {
+
+  if $::operatingsystem == 'Windows' {
+    # Datadog does not currently support Windows and macOS platforms for Network Performance Monitoring
+    fail('Network performance monitoring is only supported on Linux.')
+  }
+
+  if $service_provider {
+    service { $datadog_agent::params::sysprobe_service_name:
+      ensure    => $service_ensure,
+      enable    => $service_enable,
+      provider  => $service_provider,
+      hasstatus => false,
+      pattern   => 'dd-agent',
+      require   => Package[$datadog_agent::params::package_name],
+    }
+  } else {
+    service { $datadog_agent::params::sysprobe_service_name:
+      ensure    => $service_ensure,
+      enable    => $service_enable,
+      hasstatus => false,
+      pattern   => 'dd-agent',
+      require   => Package[$datadog_agent::params::package_name],
+    }
+  }
+
+  $sysprobe_config = {
+    'system_probe_config' => {
+      'enabled' => $enabled,
+      'sysprobe_socket' => $sysprobe_socket,
+      'log_file' => $log_file,
+    }
+  }
+
+  file { '/etc/datadog-agent/system-probe.yaml':
+    owner   => $datadog_agent::params::dd_user,
+    group   => 'dd-agent',
+    mode    => '0640',
+    content => template('datadog_agent/system_probe.yaml.erb'),
+    notify  => Service[$datadog_agent::params::sysprobe_service_name],
+    require => File['/etc/datadog-agent'],
+  }
+
+}

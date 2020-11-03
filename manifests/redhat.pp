7ea30f78c2b31a376fd266c45d2edadfd7313b1c
Pablo Baeyens <pablo.baeyens@datadoghq.com>
2020-11-03 11:35:18 +0100

    Handle keys importing through the gpgkey section The removed section was irrelevant since if keys are not available RPM will automatically import them.

--- manifests/redhat.pp
+++ manifests/redhat.pp
@@ -12,18 +12,24 @@ class datadog_agent::redhat(
 
   if $manage_repo {
 
+    $keys = [
+        'https://yum.datadoghq.com/DATADOG_RPM_KEY.public',
+        'https://yum.datadoghq.com/DATADOG_RPM_KEY_E09422B3.public',
+        'https://yum.datadoghq.com/DATADOG_RPM_KEY_20200908.public',
+    ]
+
     case $agent_major_version {
       5 : {
         $defaulturl = "https://yum.datadoghq.com/rpm/${::architecture}/"
-        $gpgkeys = ['https://yum.datadoghq.com/DATADOG_RPM_KEY.public']
+        $gpgkeys = $keys
       }
       6 : {
         $defaulturl = "https://yum.datadoghq.com/stable/6/${::architecture}/"
-        $gpgkeys = ['https://yum.datadoghq.com/DATADOG_RPM_KEY.public']
+        $gpgkeys = $keys
       }
       7 : {
         $defaulturl = "https://yum.datadoghq.com/stable/7/${::architecture}/"
-        $gpgkeys = ['https://yum.datadoghq.com/DATADOG_RPM_KEY_E09422B3.public', 'https://yum.datadoghq.com/DATADOG_RPM_KEY_20200908.public']
+        $gpgkeys = $keys[1,2]
       }
       default: { fail('invalid agent_major_version') }
     }
@@ -34,29 +40,12 @@ class datadog_agent::redhat(
       $baseurl = $defaulturl
     }
 
-    $public_key_local = '/etc/pki/rpm-gpg/DATADOG_RPM_KEY.public'
-
-    file { 'DATADOG_RPM_KEY_20200908.public':
-        owner  => root,
-        group  => root,
-        mode   => '0600',
-        path   => $public_key_local,
-        source => 'https://yum.datadoghq.com/DATADOG_RPM_KEY_20200908.public'
-    }
-
-    exec { 'install-gpg-key':
-        command => "/bin/rpm --import ${public_key_local}",
-        onlyif  => "/usr/bin/gpg --dry-run --quiet --with-fingerprint -n ${public_key_local} | grep 'C655 9B69 0CA8 82F0 23BD  F3F6 3F4D 1729 FD4B F915' || gpg --dry-run --import --import-options import-show ${public_key_local} | grep 'C6559B690CA882F023BDF3F63F4D1729FD4BF915'",
-        unless  => '/bin/rpm -q gpg-pubkey-fd4bf915',
-        require => File['DATADOG_RPM_KEY_20200908.public'],
-    }
-
     yumrepo { 'datadog-beta':
       ensure => absent,
     }
 
     yumrepo {'datadog5':
-    ensure   => absent,
+      ensure   => absent,
     }
 
     yumrepo {'datadog6':
@@ -69,7 +58,6 @@ class datadog_agent::redhat(
       gpgkey   => join($gpgkeys, "\n       "),
       descr    => 'Datadog, Inc.',
       baseurl  => $baseurl,
-      require  => Exec['install-gpg-key'],
     }
 
     package { $datadog_agent::params::package_name:

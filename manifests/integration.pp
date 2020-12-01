029ca36cdf7ef4de52c25b8baa113e53cedff94a
Albert Vaca <albert.vaca@datadoghq.com>
2020-12-01 19:44:38 +0100

    Allow removing check config files
    Adds a new 'ensure' argument to 'datadog_agent::integration' that can be
    'present' (default) or 'absent' (to remove the file).

--- manifests/integration.pp
+++ manifests/integration.pp
@@ -1,8 +1,9 @@
 define datadog_agent::integration (
-  Array $instances,
-  Optional[Hash] $init_config = undef,
-  Optional[Array] $logs       = undef,
-  String $integration         = $title,
+  Array $instances                  = [],
+  Optional[Hash] $init_config       = undef,
+  Optional[Array] $logs             = undef,
+  String $integration               = $title,
+  Enum['present', 'absent'] $ensure = 'present',
 ){
 
   include datadog_agent
@@ -20,8 +21,10 @@ define datadog_agent::integration (
     $dst = "${datadog_agent::params::legacy_conf_dir}/${integration}.yaml"
   }
 
+  $file_ensure = $ensure ? { default => file, 'absent' => absent }
+
   file { $dst:
-    ensure  => file,
+    ensure  => $file_ensure,
     owner   => $datadog_agent::dd_user,
     group   => $datadog_agent::dd_group,
     mode    => $datadog_agent::params::permissions_file,

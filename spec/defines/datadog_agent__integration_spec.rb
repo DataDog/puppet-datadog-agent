9cfe58a50a50c66712de8a3c51eeb44d9e8d2659
Albert Vaca Cintora <albert.vaca@datadoghq.com>
2020-12-02 13:21:42 +0100

    Allow removing check config files (#675)
    Adds a new 'ensure' argument to 'datadog_agent::integration' that can be
    'present' (default) or 'absent' (to remove the file).

--- spec/defines/datadog_agent__integration_spec.rb
+++ spec/defines/datadog_agent__integration_spec.rb
@@ -13,28 +13,33 @@ describe 'datadog_agent::integration' do
       end
 
       let(:title) { 'test' }
-      let(:params) do
-        {
-          instances: [
-            {
-              one: 'two',
-            },
-          ],
-        }
-      end
 
-      it { is_expected.to compile }
+      gem_spec = Gem.loaded_specs['puppet']
+
       if agent_major_version == 5
         it { is_expected.to contain_file(conf_dir.to_s).that_comes_before("File[#{conf_file}]") }
       end
-      it { is_expected.to contain_file(conf_file.to_s).with_content(%r{init_config: }) }
-      gem_spec = Gem.loaded_specs['puppet']
-      if gem_spec.version >= Gem::Version.new('4.0.0')
-        it { is_expected.to contain_file(conf_file.to_s).with_content(%r{---\ninit_config: \ninstances:\n- one: two\n}) }
-      else
-        it { is_expected.to contain_file(conf_file.to_s).with_content(%r{--- \n  init_config: \n  instances: \n    - one: two}) }
-      end
       it { is_expected.to contain_file(conf_file.to_s).that_notifies("Service[#{SERVICE_NAME}]") }
+
+      context 'with instances' do
+        let(:params) do
+          {
+            instances: [
+              {
+                one: 'two',
+              },
+            ],
+          }
+        end
+
+        it { is_expected.to compile }
+        if gem_spec.version >= Gem::Version.new('4.0.0')
+          it { is_expected.to contain_file(conf_file.to_s).with_content(%r{---\ninit_config: \ninstances:\n- one: two\n}) }
+        else
+          it { is_expected.to contain_file(conf_file.to_s).with_content(%r{--- \n  init_config: \n  instances: \n    - one: two}) }
+        end
+        it { is_expected.to contain_file(conf_file).with_ensure('file') }
+      end
 
       context 'with logs' do
         let(:params) do
@@ -48,11 +53,24 @@ describe 'datadog_agent::integration' do
           }
         end
 
+        it { is_expected.to compile }
         if gem_spec.version >= Gem::Version.new('4.0.0')
           it { is_expected.to contain_file(conf_file).with_content(%r{logs:\n- one\n- two}) }
         else
           it { is_expected.to contain_file(conf_file).with_content(%r{logs:\n  - one\n  - two}) }
         end
+        it { is_expected.to contain_file(conf_file).with_ensure('file') }
+      end
+
+      context 'with ensure absent' do
+        let(:params) do
+          {
+            ensure: 'absent',
+          }
+        end
+
+        it { is_expected.to compile }
+        it { is_expected.to contain_file(conf_file).with_ensure('absent') }
       end
     end
   end

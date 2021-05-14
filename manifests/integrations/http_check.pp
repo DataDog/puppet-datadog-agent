e5ab07f57691ceb0b243f68f74a47ee5f0a23ce1
Adrian Ding <a.ding@gns.cri.nz>
2021-05-14 23:36:55 +1200

    add support for `min_collection_interval` for HTTP check (#699)

--- manifests/integrations/http_check.pp
+++ manifests/integrations/http_check.pp
@@ -15,6 +15,10 @@
 #   method
 #    	The (optional) HTTP method. This setting defaults to GET, though many
 #    	other HTTP methods are supported, including POST and PUT.
+#   min_collection_interval
+#    	The (optional) collection interval of the check.
+#    	default: 15
+#       https://docs.datadoghq.com/developers/write_agent_check/#collection-interval
 #   data
 #       The (optional) data option. Data should be a string or an array of
 #       'key: value' pairs and will be sent in the body of the request.
@@ -170,6 +174,7 @@ class datadog_agent::integrations::http_check (
   $password  = undef,
   $timeout   = 1,
   $method    = 'get',
+  $min_collection_interval = undef,
   $data      = undef,
   $threshold = undef,
   $window    = undef,
@@ -204,6 +209,7 @@ class datadog_agent::integrations::http_check (
       'password'                     => $password,
       'timeout'                      => $timeout,
       'method'                       => $method,
+      'min_collection_interval'      => $min_collection_interval,
       'data'                         => $data,
       'threshold'                    => $threshold,
       'window'                       => $window,

dc70ad7bb5cd1e66c0b1299b70d85fb4cd7003d8
Pablo Baeyens <pablo.baeyens@datadoghq.com>
2021-08-11 12:21:32 +0200

    Use `ensure_packages` to install `gnupg`
    The `apt` module requires this since v7.2.0 which would make
    the module fail under certain situations on Ubuntu

--- manifests/ubuntu.pp
+++ manifests/ubuntu.pp
@@ -34,9 +34,7 @@ class datadog_agent::ubuntu(
   }
 
   if !$skip_apt_key_trusting {
-    package { 'gnupg':
-      ensure => installed
-    }
+    ensure_packages(['gnupg'])
 
     file { $apt_usr_share_keyring:
       ensure => file,

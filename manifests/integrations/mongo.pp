d4f1e9de735c57065bbc88cd9ac9b1de5747978e
Joe Abbate <40615740+jabbate19@users.noreply.github.com>
2022-10-04 12:08:58 -0400

    Mongo: Replace Deprecated Parameters (#752)
    * Mongo: Replace Deprecated Parameters
    
    PyMongo v3.9 and v3.12 Deprecated many ssl parameters in replace of tls
    counterparts. This was applied to the DataDog/integrations-core
    repository, but needed to be updated for pupper configs.
    
    * Add support for older agents

--- manifests/integrations/mongo.pp
+++ manifests/integrations/mongo.pp
@@ -2,6 +2,8 @@
 #
 # This class will install the necessary configuration for the mongo integration
 #
+# NOTE: In newer versions of the Datadog Agent, the ssl parameters will be deprecated in favor the tls variants
+#
 # Parameters:
 #   $additional_metrics
 #       Optional array of additional metrics
@@ -23,12 +25,20 @@
 #       Optionally specify path to SSL certificate for connection
 #   $ssl_keyfile
 #       Optionally specify path to SSL private key for connection
+#   $tls
+#       Optionally enable TLS for connection
+#   $tls_ca_file
+#       Optionally specify path to SSL/TLS Certificate Authority certificates
+#   $tls_allow_invalid_certificates
+#       Optionally require SSL/TLS client certificate for connection
+#   $tls_certificate_key_file
+#       Optionally specify path to combined SSL/TLS key and certificate for connection
 #   $tags
 #       Optional array of tags
 #   $username
 #       Optionally specify username for connection
 #
-# Sample Usage:
+# Sample Usage (Older Agent Versions):
 #
 #  class { 'datadog_agent::integrations::mongo' :
 #    servers => [
@@ -43,6 +53,33 @@
 #        'ssl_cert_reqs'      => 'CERT_REQUIRED',
 #        'ssl_certfile'       => '/path/to/client.pem',
 #        'ssl_keyfile'        => '/path/to/key.pem',
+#        'tags'               => ['optional_tag1', 'optional_tag2'],
+#        'username'           => 'mongo_username',
+#      },
+#      {
+#        'host'               => 'localhost',
+#        'port'               => '27018',
+#        'tags'               => [],
+#        'additional_metrics' => [],
+#        'collections'        => [],
+#      },
+#    ]
+#  }
+#
+# Sample Usage (Newer Agent Versions):
+#
+#  class { 'datadog_agent::integrations::mongo' :
+#    servers => [
+#      {
+#        'additional_metrics' => ['top'],
+#        'database'           => 'database_name',
+#        'host'               => 'localhost',
+#        'password'           => 'mongo_password',
+#        'port'               => '27017',
+#        'tls'                => true,
+#        'tls_ca_file'       => '/path/to/ca.pem',
+#        'tls_allow_invalid_certificates'      => false,
+#        'tls_certificate_key_file'       => '/path/to/combined.pem',
 #        'tags'               => ['optional_tag1', 'optional_tag2'],
 #        'username'           => 'mongo_username',
 #      },

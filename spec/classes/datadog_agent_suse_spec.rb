0f229f00f999b945f9e4a6074bc1ca791d4eac0b
Slavek Kabrda <slavek.kabrda@datadoghq.com>
2023-01-12 09:52:57 +0100

    Fix specs

--- spec/classes/datadog_agent_suse_spec.rb
+++ spec/classes/datadog_agent_suse_spec.rb
@@ -32,8 +32,7 @@ describe 'datadog_agent::suse' do
           .with_gpgcheck(1)\
           .with_gpgkey('https://keys.datadoghq.com/DATADOG_RPM_KEY_CURRENT.public
        https://keys.datadoghq.com/DATADOG_RPM_KEY_E09422B3.public
-       https://keys.datadoghq.com/DATADOG_RPM_KEY_FD4BF915.public
-       https://keys.datadoghq.com/DATADOG_RPM_KEY.public')\
+       https://keys.datadoghq.com/DATADOG_RPM_KEY_FD4BF915.public')\
           .with_baseurl('https://yum.datadoghq.com/suse/stable/6/x86_64')
         # .with_repo_gpgcheck(true)
       end

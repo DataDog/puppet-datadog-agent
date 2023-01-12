6b7d6e0a0370739bcd6ff134695a139bf1c42883
Kylian Serrania <kylian.serrania@datadoghq.com>
2023-01-12 09:29:16 +0100

    Linting fixes

--- test/integration/dd-agent/serverspec/default_spec.rb
+++ test/integration/dd-agent/serverspec/default_spec.rb
@@ -10,13 +10,13 @@ describe 'datadog_agent' do
     it { is_expected.to be_running }
   end
 
-  describe command('rpm -q gpg-pubkey-4172a230-55dd14f6'), :if => os[:family] == 'redhat' do
-    its(:stdout) { should match "package gpg-pubkey-4172a230-55dd14f6 is not installed" }
-    its(:exit_status) { should eq 1 }
+  describe command('rpm -q gpg-pubkey-4172a230-55dd14f6'), if: os[:family] == 'redhat' do
+    its(:stdout) { is_expected.to match 'package gpg-pubkey-4172a230-55dd14f6 is not installed' }
+    its(:exit_status) { is_expected.to eq 1 }
   end
 
-  describe command('rpm -q gpg-pubkey-4172a230-55dd14f6'), :if => os[:family] == 'opensuse' do
-    its(:stdout) { should match "package gpg-pubkey-4172a230-55dd14f6 is not installed" }
-    its(:exit_status) { should eq 1 }
+  describe command('rpm -q gpg-pubkey-4172a230-55dd14f6'), if: os[:family] == 'opensuse' do
+    its(:stdout) { is_expected.to match 'package gpg-pubkey-4172a230-55dd14f6 is not installed' }
+    its(:exit_status) { is_expected.to eq 1 }
   end
 end

1f12b9b3020e863d777b1e1b504acd0b3860b5c0
Slavek Kabrda <slavek.kabrda@datadoghq.com>
2023-01-13 14:50:17 +0100

    Properly fix CI by setting hostname in test config

--- environments/etc/manifests/site.pp
+++ environments/etc/manifests/site.pp
@@ -5,6 +5,9 @@ node default {
     agent_extra_options => {
         use_http => true,
     },
+    # Hostname is necessary for Agent to start up properly in container since 7.40.0
+    # https://github.com/DataDog/datadog-agent/issues/14152#issuecomment-1301842615
+    host                => 'some.dd.host',
     facts_to_tags       => ['osfamily'],
     integrations        => {
       'ntp' => {

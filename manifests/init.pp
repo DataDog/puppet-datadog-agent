8b1b9b423c9ca2c6d4d7e98a39d622b80436a2ab
Pythyu <antoine.zellmeyer@datadoghq.com>
2024-04-25 19:48:26 +0200

    fix(puppet): wrong string format

--- manifests/init.pp
+++ manifests/init.pp
@@ -386,7 +386,7 @@ class datadog_agent(
   case $facts['os']['name'] {
     'RedHat', 'CentOS', 'OracleLinux': {
       if $agent_version == 'latest' {
-        warn("datadog-agent ${_agent_major_version}.51 is the last supported version on CentOS 6. Installing ${_agent_major_version}.51 now")
+        notice("datadog-agent ${_agent_major_version}.51 is the last supported version on CentOS 6. Installing ${_agent_major_version}.51 now")
         $agent_version='1:7.51.1'
       } elsif $facts['os']['name']['distro']['release']['major'] < 7 and $_agent_minor_version != undef and $_agent_minor_version > 51 {
         fail("datadog-agent ${_agent_major_version}.51 is the last supported version on CentOS 6.")

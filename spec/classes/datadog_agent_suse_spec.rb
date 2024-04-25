47d99c0118419b360eca74e5fbeeecb35a7468ee
Pythyu <antoine.zellmeyer@datadoghq.com>
2024-04-25 11:36:21 +0200

    feat(puppet): expect correct gpg keys

--- spec/classes/datadog_agent_suse_spec.rb
+++ spec/classes/datadog_agent_suse_spec.rb
@@ -46,7 +46,6 @@ describe 'datadog_agent::suse' do
           .with_enabled(1)\
           .with_gpgcheck(1)\
           .with_gpgkey('https://keys.datadoghq.com/DATADOG_RPM_KEY_CURRENT.public
-       https://keys.datadoghq.com/DATADOG_RPM_KEY_E09422B3.public
        https://keys.datadoghq.com/DATADOG_RPM_KEY_FD4BF915.public
        https://keys.datadoghq.com/DATADOG_RPM_KEY_B01082D3.public')\
           .with_baseurl('https://yum.datadoghq.com/suse/stable/6/x86_64')
@@ -66,7 +65,6 @@ describe 'datadog_agent::suse' do
           .with_enabled(1)\
           .with_gpgcheck(1)\
           .with_gpgkey('https://keys.datadoghq.com/DATADOG_RPM_KEY_CURRENT.public
-       https://keys.datadoghq.com/DATADOG_RPM_KEY_E09422B3.public
        https://keys.datadoghq.com/DATADOG_RPM_KEY_FD4BF915.public
        https://keys.datadoghq.com/DATADOG_RPM_KEY_B01082D3.public')\
           .with_baseurl('https://yum.datadoghq.com/suse/stable/7/x86_64')

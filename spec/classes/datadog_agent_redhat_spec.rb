2b10a321cc8a16f750423539555092663e6c24b5
Pythyu <antoine.zellmeyer@datadoghq.com>
2024-04-25 14:30:24 +0200

    feat(puppet): fix agent_major_version when using latest

--- spec/classes/datadog_agent_redhat_spec.rb
+++ spec/classes/datadog_agent_redhat_spec.rb
@@ -34,9 +34,9 @@ describe 'datadog_agent::redhat' do
           .with_enabled(1)\
           .with_gpgcheck(1)\
           .with_gpgkey('https://keys.datadoghq.com/DATADOG_RPM_KEY_CURRENT.public
-       https://keys.datadoghq.com/DATADOG_RPM_KEY_E09422B3.public
        https://keys.datadoghq.com/DATADOG_RPM_KEY_FD4BF915.public
-       https://keys.datadoghq.com/DATADOG_RPM_KEY_B01082D3.public')\
+       https://keys.datadoghq.com/DATADOG_RPM_KEY_B01082D3.public
+       https://keys.datadoghq.com/DATADOG_RPM_KEY_E09422B3.public')\
           .with_baseurl('https://yum.datadoghq.com/rpm/x86_64/')\
           .with_repo_gpgcheck(false)
       end

dbccc374f43b2a92aca4d3bbf8313586a7c1b537
Pythyu <antoine.zellmeyer@datadoghq.com>
2024-04-26 16:09:08 +0200

    feat(puppet): pin to 7.51.1 on centos 6 only

--- manifests/init.pp
+++ manifests/init.pp
@@ -385,7 +385,7 @@ class datadog_agent(
 
   case $facts['os']['name'] {
     'RedHat', 'CentOS', 'OracleLinux': {
-      if $agent_version == 'latest' {
+      if $facts['os']['release']['full'] =~ /^6(.[0-9])?/ and $agent_version == 'latest' {
         notice("datadog-agent ${_agent_major_version}.51 is the last supported version on CentOS 6. Installing ${_agent_major_version}.51 now")
         $agent_full_version='7.51.1'
       } elsif $facts['os']['release']['full'] =~ /^6(.[0-9])?/ and $_agent_minor_version != undef and $_agent_minor_version > 51 {

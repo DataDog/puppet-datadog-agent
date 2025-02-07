24447cececeab9c97eff1efcef3e503437fe8fdd
Darren Gipson <darren.gipson@towerswatson.com>
2025-02-07 14:02:10 +0000

    fix tests to match default values

--- spec/classes/datadog_agent_integrations_http_check_spec.rb
+++ spec/classes/datadog_agent_integrations_http_check_spec.rb
@@ -38,19 +38,19 @@ describe 'datadog_agent::integrations::http_check' do
 
         it { is_expected.to contain_file(conf_file).without_content(%r{username: }) }
         it { is_expected.to contain_file(conf_file).without_content(%r{password: }) }
-        it { is_expected.to contain_file(conf_file).without_content(%r{timeout: 1}) }
+        it { is_expected.to contain_file(conf_file).with_content(%r{timeout: 1}) } # default value is 1
         it { is_expected.to contain_file(conf_file).without_content(%r{data: }) }
         it { is_expected.to contain_file(conf_file).without_content(%(threshold: )) }
         it { is_expected.to contain_file(conf_file).without_content(%r{window: }) }
         it { is_expected.to contain_file(conf_file).without_content(%r{content_match: }) }
-        it { is_expected.to contain_file(conf_file).without_content(%r{reverse_content_match: true}) }
+        it { is_expected.to contain_file(conf_file).with_content(%r{reverse_content_match: false}) } # default value is false
         it { is_expected.to contain_file(conf_file).without_content(%r{include_content: true}) }
-        it { is_expected.to contain_file(conf_file).without_content(%r{collect_response_time: true}) }
+        it { is_expected.to contain_file(conf_file).with_content(%r{collect_response_time: true}) } # default value is true
         it { is_expected.to contain_file(conf_file).without_content(%r{http_response_status_code: }) }
-        it { is_expected.to contain_file(conf_file).without_content(%r{disable_ssl_validation: false}) }
-        it { is_expected.to contain_file(conf_file).without_content(%r{skip_event: true }) }
-        it { is_expected.to contain_file(conf_file).without_content(%r{no_proxy: false }) }
-        it { is_expected.to contain_file(conf_file).without_content(%r{check_certificate_expiration: true }) }
+        it { is_expected.to contain_file(conf_file).with_content(%r{disable_ssl_validation: false}) } # default value is false
+        it { is_expected.to contain_file(conf_file).with_content(%r{skip_event: true}) } # default value is true
+        it { is_expected.to contain_file(conf_file).with_content(%r{no_proxy: false}) } # default value is false
+        it { is_expected.to contain_file(conf_file).with_content(%r{check_certificate_expiration: true}) } # default value is true
         it { is_expected.to contain_file(conf_file).without_content(%r{days_warning: }) }
         it { is_expected.to contain_file(conf_file).without_content(%r{days_critical: }) }
         it { is_expected.to contain_file(conf_file).without_content(%r{headers: }) }

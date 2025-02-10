8fafd0738fc73c962ee10915f850b9a90b337765
Darren Gipson <darren.gipson@towerswatson.com>
2025-02-10 08:38:49 +0000

    move to non deprecated function for ensure_packages

--- manifests/ubuntu.pp
+++ manifests/ubuntu.pp
@@ -33,7 +33,7 @@ class datadog_agent::ubuntu (
   }
 
   if !$skip_apt_key_trusting {
-    ensure_packages(['gnupg'])
+    stdlib::ensure_packages(['gnupg'])
 
     file { $apt_usr_share_keyring:
       ensure => file,
